51beb298898eee3914235cee8ce69828c1eb089f
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-07-03 17:56:34 -0700

    initialize nodemailer

--- models/authentication.js
+++ models/authentication.js
@@ -1,3 +1,8 @@
+//Require dotenv file
+require ('dotenv').config();
+//Require Nodemailer 
+const nodemailer = requie("nodemailer");
+
 // INIT
 var myString = "blablabla Card game bla";
 var myPassword = "myPassword";
@@ -9,3 +14,11 @@ document.getElementById("demo0").innerHTML = myString;
 document.getElementById("demo1").innerHTML = encrypted;
 document.getElementById("demo2").innerHTML = decrypted;
 document.getElementById("demo3").innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: MY_GMAIL_ADDRESS, 
+        pass: MY_GMAIL_PASSWORD   }
+})
+
